244af6b5c4d02da843e7f52e1fb96d33ca9db690
dlangdon <daniel.langdon@gmail.com>
2013-05-11 01:51:59 -0400

    Added trust levels between empires.

--- src/state/Empire.java
+++ src/state/Empire.java
@@ -1,5 +1,6 @@
 package state;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -7,11 +8,19 @@ import org.newdawn.slick.Color;
 
 public class Empire
 {
+// Statics ============================================================================================================	
+	public static final int CEASE_FIRE = 17;
+	public static final int OPEN_TRADE = 34;
+	public static final int CULTURAL_COOPERATION = 50;
+	public static final int SANCTUARY = 67;
+	public static final int RESOURCE_SHARING = 84;
+	
 // Internals ==========================================================================================================
 	private String name_;
 	private Color color_;
 	private Economy economy_;
 	private HashSet<Colony> colonies_;
+	private HashMap<Empire, Integer> trust;
 
 // Public Methods =====================================================================================================
 
@@ -22,6 +31,7 @@ public class Empire
 		this.color_ = color_;
 		colonies_ = new HashSet<Colony>();
 		economy_ = new Economy();
+		trust = new HashMap<Empire, Integer>();
 	}
 
 	public void addColony(Colony colony)
@@ -68,4 +78,24 @@ public class Empire
 		return colonies_;
 	}
 
+	/**
+	 * Returns the level of trust this empire is willing to confer to the other empire.
+	 * @param other Opposite empire.
+	 * @return A number between 0 and 100 measuring trust.
+	 */
+	public int trustLevel(Empire other)
+	{
+		Integer aux = trust.get(other);
+		return aux == null ? 0 : aux;
+	}
+	
+	/**
+	 * Returns the level of reciprocal truth between two empires.
+	 * @param other The other empire.
+	 * @return A number between 0 and 100 measuring trust.
+	 */
+	public int reciprocalTrust(Empire other)
+	{
+		return Math.min(this.trustLevel(other), other.trustLevel(this));
+	}
 }
