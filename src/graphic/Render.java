dce3721b14af7777b26b7e7b6b01b257bc304fad
dlangdon <daniel.langdon@gmail.com>
2014-01-13 23:30:44 -0800

    Updates on UI designs and font rendering.

--- src/graphic/Render.java
+++ src/graphic/Render.java
@@ -2,6 +2,9 @@ package graphic;
 
 import java.awt.Font;
 
+import org.newdawn.slick.AngelCodeFont;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.TrueTypeFont;
 
 /**
@@ -19,11 +22,17 @@ public class Render
 	
 	public static TrueTypeFont titles;
 	public static TrueTypeFont normal;
+	public static AngelCodeFont dialogTitle;
+	public static AngelCodeFont dialogSubTitle;
+	public static AngelCodeFont dialogText;
 
-	public static void init()
+	public static void init() throws SlickException
 	{
 		titles = new TrueTypeFont(new Font("Arial", Font.BOLD, 16), false);
 		normal = new TrueTypeFont(new Font("Arial", Font.PLAIN, 12), false);
+		dialogTitle = new AngelCodeFont("resources/fonts/space_age_30.fnt", "resources/fonts/space_age_30_0.png");
+		dialogSubTitle = new AngelCodeFont("resources/fonts/space_age_20.fnt", "resources/fonts/space_age_20_0.png");
+		dialogText = new AngelCodeFont("resources/fonts/neogrey_12.fnt", "resources/fonts/neogrey_12_0.png");
 	}
 	
 }
--- src/ui/EconomyDialog.java
+++ src/ui/EconomyDialog.java
@@ -2,9 +2,7 @@ package ui;
 
 import graphic.Render;
 
-import org.newdawn.slick.Color;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
+import org.newdawn.slick.*;
 
 import empire.Economy;
 import empire.Empire;
@@ -13,11 +11,13 @@ public class EconomyDialog
 {
 // Internals ==========================================================================================================	
 	private boolean visible;
-	
+	private Image background;
+
 // Public Methods =====================================================================================================
-	public EconomyDialog()
+	public EconomyDialog() throws SlickException
 	{
 		visible = false;
+		background = new Image("resources/dialog_base_complete.png");
 	}
 	
 	public void render(GameContainer gc, Graphics g)
@@ -29,27 +29,30 @@ public class EconomyDialog
 		Economy ec = e.getEconomy();
 
 		// Background.
-		g.setColor(new Color(0.0f, 0.0f, 0.0f, 0.8f));
-		g.fillRect(50, 50, 500, 500);
+		background.draw(3, 3);
+		//g.setColor(new Color(0.0f, 0.0f, 0.0f, 0.8f));
+		//g.fillRect(50, 50, 500, 500);
+
 
 		// Policy
-		int y = 50;
-		Render.titles.drawString(100, (y+=20), "Policy:");
-		Render.normal.drawString(120, (y+=20), String.format("Limit growth to %d%% of production.", (int)(ec.growthPolicy()*100)));
-		Render.normal.drawString(120, (y+=15), String.format("Only spend on infrastructure if costs are recovered before %d turns.", (ec.returnOfInvestmentLimit())));
-		Render.normal.drawString(120, (y+=15), (ec.isOnlyLocal() ? "Allow" : "Prohibit") + " spending of reserve to boost growth (at 50% extra cost)");
+		int y = 20;
+		int x = 50;
+		Render.dialogTitle.drawString(x+350, (y+=220), "Policy:");
+		Render.normal.drawString(x + 70, (y+=20), String.format("Limit growth to %d%% of production.", (int)(ec.growthPolicy()*100)));
+		Render.normal.drawString(x + 70, (y+=15), String.format("For investments recovered before %d turns.", (ec.returnOfInvestmentLimit())));
+		Render.normal.drawString(x + 70, (y+=15), (ec.isOnlyLocal() ? "Allow" : "Prohibit") + " spending of reserve to boost growth (at 50% extra cost)");
 
 		// Income
-		y += 20;
-		Render.titles.drawString(100, y, "Last turn income:");
+		y += 50;
+		Render.titles.drawString(x, y, "Last turn income:");
 		y += 20;
 		float totalIncome = 0.0f;
 		for(int i=0; i<Economy.causes().size(); i++)
 		{
 			if(ec.movements()[i] > 0)
 			{
-				Render.normal.drawString(120, y, Economy.causes().get(i));
-				Render.normal.drawString(320, y, String.format("$ %10d %s", (int)(ec.movements()[i]*10000.0f), ec.rejections()[i] ? "!" : ""));
+				Render.normal.drawString(x + 20, y, Economy.causes().get(i));
+				Render.normal.drawString(x + 220, y, String.format("$ %10d %s", (int)(ec.movements()[i]*10000.0f), ec.rejections()[i] ? "!" : ""));
 				y += 15;
 				totalIncome += ec.movements()[i];
 			}
@@ -57,30 +60,30 @@ public class EconomyDialog
 		
 		// Expenses
 		y += 20;
-		Render.titles.drawString(100, y, "Last turn expenses:");
+		Render.titles.drawString(x, y, "Last turn expenses:");
 		y += 20;
 		float totalExpenses = 0.0f;
 		for(int i=0; i<Economy.causes().size(); i++)
 		{
 			if(ec.movements()[i] <= 0)
 			{
-				Render.normal.drawString(120, y, Economy.causes().get(i));
-				Render.normal.drawString(320, y, String.format("$ %10d %s", (int)(-ec.movements()[i]*10000.0f), ec.rejections()[i] ? "!" : ""));
+				Render.normal.drawString(x + 20, y, Economy.causes().get(i));
+				Render.normal.drawString(x + 220, y, String.format("$ %10d %s", (int)(-ec.movements()[i]*10000.0f), ec.rejections()[i] ? "!" : ""));
 				y += 15;
 				totalExpenses += ec.movements()[i];
 			}
 		}
 		
 		// Totals
-		Render.titles.drawString(100, (y+=20), "Totals:");
-		Render.normal.drawString(120, (y+=20), "Income");
-		Render.normal.drawString(320, y, String.format("$ %10d", (int)(totalIncome*10000.0f) ));
-		Render.normal.drawString(120, (y+=15), "Expenses");
-		Render.normal.drawString(320, y, String.format("$ %10d", (int)(totalExpenses*10000.0f)));
-		Render.normal.drawString(120, (y+=15), "To Imperial Reserve");
-		Render.normal.drawString(320, y, String.format("$ %10d", (int)((totalIncome + totalExpenses)*10000.0f)));
-		Render.normal.drawString(120, (y+=15), "Current Reserve");
-		Render.normal.drawString(320, y, String.format("$ %10d", (int)(ec.reserve()*10000.0f)));
+		Render.titles.drawString(x, (y+=20), "Totals:");
+		Render.normal.drawString(x + 20, (y+=20), "Income");
+		Render.normal.drawString(x + 220, y, String.format("$ %10d", (int)(totalIncome*10000.0f) ));
+		Render.normal.drawString(x + 20, (y+=15), "Expenses");
+		Render.normal.drawString(x + 220, y, String.format("$ %10d", (int)(totalExpenses*10000.0f)));
+		Render.normal.drawString(x + 20, (y+=15), "To Imperial Reserve");
+		Render.normal.drawString(x + 220, y, String.format("$ %10d", (int)((totalIncome + totalExpenses)*10000.0f)));
+		Render.normal.drawString(x + 20, (y+=15), "Current Reserve");
+		Render.normal.drawString(x + 220, y, String.format("$ %10d", (int)(ec.reserve()*10000.0f)));
 	}
 
 	/**
